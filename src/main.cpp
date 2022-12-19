40387f635cb0c18d2c78d4c550958183ea2740b9
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-12-20 01:17:47 +0900

    fix color system

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,15 @@
 #include <stdio.h>
 #include <GL/glut.h>
-#include "../inc/ConvertColor.hpp"
+#include "../inc/Color255.hpp"
 
 int main(int argc, char** argv)
 {
-    GLSYS::ConvertColor col(100,255,30,20); // r g b a
-    printf("%f, %f, %f, %f\n", col.GetColor().r, col.GetColor().g, col.GetColor().b, col.GetColor().a);
+    Color255 col;
+    col = Color255("#2549C4");
+    printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
+    col = Color255("141414");
+    printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
+    col = Color255(253,45,32,25);
+    printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
     return 0;
 }
