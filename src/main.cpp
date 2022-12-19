16b00cb5b636c870714d529e4ce5e57651b9ccb6
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-12-20 02:18:27 +0900

    動作確認用画面提示

--- src/main.cpp
+++ src/main.cpp
@@ -4,20 +4,27 @@
 #include "inc/SceneManager.hpp"
 #include "inc/TitleScene.hpp"
 
-int main(int argc, char** argv)
+void display(){}
+
+int main(int argc, char **argv)
 {
     Color255 col;
     col = Color255("#2549C4");
     printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
     col = Color255("141414");
     printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
-    col = Color255(253,45,32,25);
+    col = Color255(253, 45, 32, 25);
     printf("%f, %f, %f, %f\n", col.r, col.g, col.b, col.a);
 
     Scene::TitleScene *titleScene = new Scene::TitleScene();
     Scene::SceneManager::ChangeScene(titleScene);
 
     Scene::SceneManager::Update();
+
+    glutInit(&argc, argv);
+    glutCreateWindow(argv[0]);
+    glutDisplayFunc(display);
+    glutMainLoop();
 
     return 0;
 }
