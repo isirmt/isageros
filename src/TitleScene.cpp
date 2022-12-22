2f4c0cbba716b69ae24da09f9eae41391ef4840e
isirmt <r119306@g.kobe-kosen.ac.jp>
2022-12-22 16:34:54 +0900

    Test Color

--- src/TitleScene.cpp
+++ src/TitleScene.cpp
@@ -14,11 +14,12 @@ void Scene::TitleScene::Draw() {
   glTranslatef(0.0, 5., 0.0);
   glRotatef(deg, 1.0, 0.7, 0.0);
   glScalef(12, 10, 12);
-  glEnable(GL_NORMALIZE);  // スケールの際に必要。よう検索
-  ambient[0] = 114. / 255.;
-  ambient[1] = 218. / 255.;
-  ambient[2] = 209. / 255.;
-  ambient[3] = 1.0;
+  glEnable(GL_NORMALIZE);
+  boxAmbient = Color255(114, 235, 209);
+  ambient[0] = boxAmbient.r;
+  ambient[1] = boxAmbient.g;
+  ambient[2] = boxAmbient.b;
+  ambient[3] = boxAmbient.a;
   diffuse[0] = .3;
   diffuse[1] = .3;
   diffuse[2] = .3;
--- src/inc/TitleScene.hpp
+++ src/inc/TitleScene.hpp
@@ -1,24 +1,25 @@
 #pragma once
-#include "SceneBase.hpp"
 #include <GL/glut.h>
 
-namespace Scene
-{
-    class TitleScene
-        : public SceneBase
-    {
-    public:
-        TitleScene() {deg = 0.f;}
-        ~TitleScene() {}
+#include "SceneBase.hpp"
+#include "Color255.hpp"
 
-        void Update() override;
-        void Draw() override;
-        void KeyboardProc(unsigned char key, int x, int y) override {}
-        void MouseProc(int button, int state, int x, int y) override {}
-        void MotionProc(int x, int y) override {}
-        void PassiveMotionProc(int x, int y) override {}
-        void SpecialFuncProc(int key, int x, int y) override {}
-        private:
-        float deg;
-    };
+namespace Scene {
+class TitleScene : public SceneBase {
+ public:
+  TitleScene() { deg = 0.f; }
+  ~TitleScene() {}
+
+  void Update() override;
+  void Draw() override;
+  void KeyboardProc(unsigned char key, int x, int y) override {}
+  void MouseProc(int button, int state, int x, int y) override {}
+  void MotionProc(int x, int y) override {}
+  void PassiveMotionProc(int x, int y) override {}
+  void SpecialFuncProc(int key, int x, int y) override {}
+
+ private:
+  float deg;
+  Color255 boxAmbient;
+};
 }
