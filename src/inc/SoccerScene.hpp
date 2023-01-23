1e0ec55a52187b219615318a55d1a2581c260c5e
jakopii <h.n.jakopii@gmail.com>
2023-01-23 22:14:34 +0900

    SoccerSceneのhppできた

---
+++ src/inc/SoccerScene.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <GL/glut.h>
+
+#include <iostream>
+
+#include "2DAssets.hpp"
+#include "3DAssets.hpp"
+#include "Color255.hpp"
+#include "LightManager.hpp"
+#include "SceneBase.hpp"
+#include "MouseInput.hpp"
+#include "SceneManager.hpp"
+
+namespace Scene{
+    class SoccerScene : public SceneBase{
+        public:
+            SoccerScene();
+            ~SoccerScene(){
+                delete backbutton;
+            }
+
+        void Update() override;
+            void Draw() override;
+            void KeyboardProc(unsigned char key, int x, int y) override{}
+            void MouseProc(int button, int state, int x, int y) override{}
+            void MotionProc(int x, int y) override{}
+            void PassiveMotionProc(int x, int y) override{}
+            void SpecialFuncProc(int key, int x, int y) override{}
+
+        private:
+            Obj::Button* backbutton;
+    }
+}
